f0d248e99ae95cfb3ce28f9aa53a51b90fb5457b
djh <dengjinghui@shiyue.com>
2021-09-07 17:19:25 +0800

    this is demo

---
+++ src/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Test.php
+ * @author      djh <dengjinghui@shiyue.com>
+ * @date     2021/9/7 16:48
+ * PhpStorm
+ */
+
+
+namespace Cachesystem;
+
+class Test
+{
+    public function index()
+    {
+        echo '11';
+    }
+
+}
---
+++ ttt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ttt.php
+ * @author   djh <dengjinghui@shiyue.com>
+ * @date     2021/9/7
+ * PhpStorm
+ * @desc:
+ */
+
+require_once "./vendor/autoload.php";
+
+use CacheSystem\Test;
+
+$a = new Test();
+ $a->index();
